d00d981ac2783f02269e95b708ad72365e170496
selomelo90 <selim.boyukk@gmail.com>
2026-02-16 11:31:12 +0300

    chore: cleanup middleware

--- packages/web/middleware.ts
+++ packages/web/middleware.ts
@@ -1,12 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default async function middleware(request: any) {
-  console.log('Middleware routing:', request.nextUrl.pathname);
-  const handleI18nRouting = createMiddleware(routing);
-  const response = await handleI18nRouting(request);
-  return response;
-}
+export default createMiddleware(routing);
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)']
