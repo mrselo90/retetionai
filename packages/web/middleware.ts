3399268a15be863cbe0e9089de6532c6587182f4
selomelo90 <selim.boyukk@gmail.com>
2026-02-16 11:25:40 +0300

    debug: add logs to middleware

--- packages/web/middleware.ts
+++ packages/web/middleware.ts
@@ -1,11 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default async function middleware(request: any) {
+  console.log('Middleware routing:', request.nextUrl.pathname);
+  const handleI18nRouting = createMiddleware(routing);
+  const response = await handleI18nRouting(request);
+  return response;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
